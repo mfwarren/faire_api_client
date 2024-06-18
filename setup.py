247736dec2e33a814319f892d8aa8767821486c3
Matt Warren <matt.warren@gmail.com>
2024-06-18 19:52:25 -0400

    name with underscores

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="faire-api-client",
+    name="faire_api_client",
     version="0.0.1",
     author="Steven Athouel",
     author_email="sathouel@gmail.com",
